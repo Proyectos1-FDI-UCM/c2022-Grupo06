76ced867c1bf4008c09cadc674a95664fecca0bd
JoseMorenob <100274773+JoseMorenob@users.noreply.github.com>
2022-03-04 11:34:05 +0100

    REJILLA SUPER BIEEEEN
    <3 estoy proud

--- Nombre del juego/Assets/scripts/Platform/Rejilla_Platform_Component.cs
+++ Nombre del juego/Assets/scripts/Platform/Rejilla_Platform_Component.cs
@@ -9,9 +9,8 @@ public class Rejilla_Platform_Component : MonoBehaviour
     private Rigidbody2D _playerRigidbody;
     private Collider2D _myCollider;
     private float _impulse = 35.0f;
-    private float _timer = 0.0000020f;
-    private float _currentimer = 0;
     private bool _fisrtime = true;
+    private Player_Life_Component _player;
     #endregion
     // Start is called before the first frame update
     void Start()
@@ -21,45 +20,28 @@ public class Rejilla_Platform_Component : MonoBehaviour
         _myCollider.isTrigger = true;
 
     }
-    void OnTriggerEnter2D(Collider2D collision)
+    void OnTriggerStay2D(Collider2D collision)
     {
 
-        Player_Life_Component _player = collision.gameObject.GetComponent<Player_Life_Component>();
-        if (_player != null && _fisrtime)
+        if (collision.gameObject.GetComponent<Player_Life_Component>() && _fisrtime)
         {
-
             _myRigidbody.simulated = false;
-            _playerRigidbody = _player.GetComponent<Rigidbody2D>();
+            _playerRigidbody = collision.gameObject.GetComponent<Rigidbody2D>();
             _playerRigidbody.velocity = new Vector2(0, _impulse);
-            _currentimer = _timer;
             _fisrtime = false;
         }
 
     }
    void OnTriggerExit2D(Collider2D Collision)
     {
-        Debug.Log("exittttt");
-        Player_Life_Component _player = Collision.gameObject.GetComponent<Player_Life_Component>();
-        if (_player != null)
+ 
+        
+        if (Collision.gameObject.GetComponent<Player_Life_Component>())
         {
             _myRigidbody.simulated = true;
             _myCollider.isTrigger = false;
         }
     }
-    void Update()
-    {
-        if (_currentimer > 0)
-        {
-            Debug.Log("ein");
-            Debug.Log(_currentimer);
-            _currentimer -= Time.deltaTime;
-        }
-        else if (_currentimer < 00.00000010f)
-        {
-            Debug.Log("entra?");
-            _myRigidbody.simulated = true;
-            _currentimer = 0;
-        }
-    }
+   
 
 }
