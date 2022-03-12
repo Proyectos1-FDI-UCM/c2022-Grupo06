7ea60f2c8625fae26829bd227178f6a946df37ed
cristina <Crmora03@ucm.es>
2022-03-12 17:53:38 +0100

    main menu

--- Nombre del juego/Assets/scripts/GameManager.cs
+++ Nombre del juego/Assets/scripts/GameManager.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    static private GameManager _instance;
+    static public GameManager Instance
     {
-        
+        get
+        {
+            return _instance;
+        }
+    }
+    
+    [SerializeField]
+    private GameObject _player;
+    [SerializeField]
+    private GameObject _bow;
+    [SerializeField]
+    private GameManager _gameManager;
+    private void Awake()
+    {
+        _instance = this;
+    }
+    public void StartMatch()
+    {
+        _player.SetActive(true);
+        _bow.SetActive(true);
+        UIManager.Instance.SetMainMenu(false);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        _player.SetActive(false);
+        _bow.SetActive(false);
+        UIManager.Instance.SetMainMenu(true);
     }
 }
--- Nombre del juego/Assets/scripts/UIManager.cs
+++ Nombre del juego/Assets/scripts/UIManager.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+
+    [SerializeField]
+    private GameObject _mainMenu;
+    static private UIManager _instance;
+    static public UIManager Instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+    public void SetMainMenu(bool enabled)
+    { //permite activar o desactivar el menu inicial
+        _mainMenu.SetActive(enabled);
+    }
+    public void StartMatch()
+    {
+        GameManager.Instance.StartMatch();
+    }
+    public void QuitGame()
+    {
+        GameManager.Instance.QuitGame();
+    }
+    private void Awake()             //como el GameManager, debemos inicializarlo a lo primero para que vaya preparando todo lo necesario para el juego
+    {
+        _instance = this;
+    }
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
