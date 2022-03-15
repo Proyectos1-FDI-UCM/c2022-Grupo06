99dc7f5be1066523f8a1b17570dc20c6fa908e5c
cristina <Crmora03@ucm.es>
2022-03-15 17:22:01 +0100

    camara y enemigos en menu

--- Nombre del juego/Assets/scripts/GameManager.cs
+++ Nombre del juego/Assets/scripts/GameManager.cs
@@ -19,6 +19,10 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     private GameObject _enemyMov;
     [SerializeField]
+    private GameObject _enemyDisp;
+    [SerializeField]
+    private GameObject _Camera;
+    [SerializeField]
     private GameManager _gameManager;
     [SerializeField]
     private Player_Life_Component _myPlayer_Life_Component;
@@ -31,9 +35,12 @@ public class GameManager : MonoBehaviour
     }
     public void StartMatch()
     {
+        //activa cada uno de los personajes, el mov de la cámara y quita el menú principal
         _player.SetActive(true);
         _bow.SetActive(true);
         _enemyMov.SetActive(true);
+        _enemyDisp.SetActive(true);
+        _Camera.GetComponent<CamaraMovement>().enabled = true;
         UIManager.Instance.SetMainMenu(false);
     }
     public void QuitGame()
@@ -51,9 +58,12 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         Time.timeScale = 0.0f;                              //falta desactivar el componente de la cámra      
+       
         _player.SetActive(false);
         _bow.SetActive(false);
         _enemyMov.SetActive(false);
+        _enemyDisp.SetActive(false);
+        _Camera.GetComponent<CamaraMovement>().enabled = false;
         UIManager.Instance.SetMainMenu(true);
     }
    
