3f0c99a13d8075f6a55428c15999c8209aacd4c3
CiscoGalvan <100274715+CiscoGalvan@users.noreply.github.com>
2022-04-22 12:50:48 +0200

    Nueva Prueba

--- Nombre del juego/Assets/scripts/ActivarManager.cs
+++ Nombre del juego/Assets/scripts/ActivarManager.cs
@@ -6,6 +6,11 @@ public class ActivarManager : MonoBehaviour
 {
     [SerializeField]
     private GameObject _randomManager;
+
+    [SerializeField]
+    private GameObject _ultimaLuz;
+    [SerializeField]
+    private GameObject _ultimocllider;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.GetComponent<PlayerInput_Component>())
@@ -13,6 +18,8 @@ public class ActivarManager : MonoBehaviour
             Debug.Log("entra");
             _randomManager.SetActive(true);
             this.gameObject.SetActive(false);
+            _ultimaLuz.SetActive(true);
+            _ultimocllider.SetActive(true);
         }
     }
 }
